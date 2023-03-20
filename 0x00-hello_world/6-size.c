dcf5c1e2cc874cb9568416cd1f19c6cb128520f9
Uchenna Onele <unclefab100@gmail.com>
2023-03-20 12:06:54 -0700

    processor

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char c;
+float f;
+long long lli;
+long li;
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+return (0);
