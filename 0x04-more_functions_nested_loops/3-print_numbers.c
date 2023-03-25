c3c950b748bf7d4f9a53ca471bc81575295bf0fc
Uchenna Onele <unclefab100@gmail.com>
2023-03-24 21:23:34 -0700

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
