329fbe8a59bbdf94f1b31d274d0d3546aefdc21a
Uchenna Onele <unclefab100@gmail.com>
2023-03-27 13:16:42 -0700

    put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts -  prints a string, followed by a new line, to stdout
+ * @str: holds the string
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
