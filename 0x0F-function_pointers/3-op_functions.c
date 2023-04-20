b2e4c83c09c6ced0c2516f34d07cd761b4714978
Uchenna Onele <unclefab100@gmail.com>
2023-04-20 00:21:42 -0700

    3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -55,3 +55,20 @@ int op_div(int a, int b)
 
 	return (a / b);
 }
+/**
+  * op_mod - ...
+  * @a: ...
+  * @b: ...
+  *
+  * Return: ...
+  */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
